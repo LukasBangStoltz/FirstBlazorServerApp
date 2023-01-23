610194354d7ed0f540e35536904b08b73b075a2b
Lukas Bang Stoltz-Andersen <lukas.stoltzandersen@formpipe.com>
2023-01-23 10:16:46 +0100

    first commit

---
+++ BaseComponents/DateTimeBaseComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FirstBlazorServerApp.BaseComponents
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; } = DateTime.Now;
+    }
+}
